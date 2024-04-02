b809666af88459d7bf0d21b20a2205a2805b0e9d
daotq1 <v.daotq1@vinhomes.vn>
2024-04-02 17:45:55 +0700

    update

---
+++ aws-resources/20_code-build.tf
@@ -0,0 +1,32 @@
+resource "aws_codebuild_project" "example" {
+  name          = "aws-iaac-project"
+  description   = "Project for AWS CodeBuild"
+  build_timeout = "5"  # Duration in minutes
+  service_role  = var.aws_role_code_pipeline  # Ensure you've defined the IAM role resource
+
+  artifacts {
+    type = "S3"
+    artifact_identifier = aws_s3_bucket.bucket_data
+  }
+
+  environment {
+    compute_type                = "Lambda"  # Replace with your preferred type
+    image                       = "Java"  # Replace with your preferred image
+    type                        = "Amazon Linux"
+    image_pull_credentials_type = "aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto11"
+    registry_credential {
+      credential          = var.registry_credential
+      credential_provider = var.registry_credential_provider
+    }
+    environment_variable {
+      name  = ""
+      value = ""
+    }
+  }
+
+  source {
+    type      = "GITHUB"  # Replace with your source repository type
+    location  = "https://github.com/user/repo.git"  # Replace with your repository URL
+  }
+
+}
--- aws-resources/variables.tf
+++ aws-resources/variables.tf
@@ -6,3 +6,19 @@ variable "ssh_access_key" {
   type = string
   default = "public-bastion-host"
 }
+variable "aws_role_code_pipeline" {
+  type = string
+  default = "aws-code-pipeline"
+}
+variable "registry_credential" {
+  type = string
+  default = "registry_credential"
+}
+variable "registry_credential_provider" {
+  type = string
+  default = "registry_credential_provider"
+}
+variable "github_repsitory" {
+  type = string
+  default = "https://github.com/daotq2000/aws-iaac-terraform"
+}
