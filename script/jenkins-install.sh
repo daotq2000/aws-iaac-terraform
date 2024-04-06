e6d8521df3bd1b441046be50ca40f3d57aabc8aa
TRAN QUANG DAO <daotq2@gmail.com>
2024-04-06 22:33:31 +0700

    done all infastructure

---
+++ script/jenkins-install.sh
@@ -0,0 +1,9 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins.service
+sudo systemctl status jenkins
+sudo ufw allow 8080
+sudo ufw allow OpenSSH
+sudo ufw enable
