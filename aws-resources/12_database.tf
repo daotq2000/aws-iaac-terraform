59f61b681fbe6a8615116f946a9dbb8d98e2312d
TRAN QUANG DAO <daotq2@gmail.com>
2024-06-20 23:38:30 +0700

    update

--- aws-resources/12_database.tf
+++ aws-resources/12_database.tf
@@ -1,37 +1,37 @@
-resource "aws_rds_cluster" "aurora-postgresql-cluster" {
-  cluster_identifier = "aurora-postgresql-cluster"
-  engine             = "aurora-postgresql"
-  engine_mode        = "provisioned"
-  engine_version     = "13.7"
-  database_name      = "test"
-  master_username    = "test"
-  master_password    = "sadalkdakl232"
-  vpc_security_group_ids = [aws_security_group.postgres_aurora_sg.id] # Replace with your security group IDs
-  db_subnet_group_name   = aws_db_subnet_group.subnet_group.name
-  storage_encrypted  = true
-  skip_final_snapshot = true
-  serverlessv2_scaling_configuration {
-    max_capacity = 1.0
-    min_capacity = 0.5
-  }
-}
-
-resource "aws_rds_cluster_instance" "aurora-postgresql-cluster" {
-  for_each = var.az
-  identifier = "instance-${each.value}"
-  cluster_identifier = aws_rds_cluster.aurora-postgresql-cluster.id
-  instance_class     = "db.serverless"
-  engine             = aws_rds_cluster.aurora-postgresql-cluster.engine
-  engine_version     = aws_rds_cluster.aurora-postgresql-cluster.engine_version
-  availability_zone = each.value
-}
-
+# resource "aws_rds_cluster" "aurora-postgresql-cluster" {
+#   cluster_identifier = "aurora-postgresql-cluster"
+#   engine             = "aurora-postgresql"
+#   engine_mode        = "provisioned"
+#   engine_version     = "13.7"
+#   database_name      = "test"
+#   master_username    = "test"
+#   master_password    = "sadalkdakl232"
+#   vpc_security_group_ids = [aws_security_group.postgres_aurora_sg.id] # Replace with your security group IDs
+#   db_subnet_group_name   = aws_db_subnet_group.subnet_group.name
+#   storage_encrypted  = true
+#   skip_final_snapshot = true
+#   serverlessv2_scaling_configuration {
+#     max_capacity = 1.0
+#     min_capacity = 0.5
+#   }
+# }
+#
 # resource "aws_rds_cluster_instance" "aurora-postgresql-cluster" {
-# #   for_each = var.az
-#   identifier = "instance-us-east-1a"
+#   for_each = var.az
+#   identifier = "instance-${each.value}"
 #   cluster_identifier = aws_rds_cluster.aurora-postgresql-cluster.id
 #   instance_class     = "db.serverless"
 #   engine             = aws_rds_cluster.aurora-postgresql-cluster.engine
 #   engine_version     = aws_rds_cluster.aurora-postgresql-cluster.engine_version
-#   availability_zone = "us-east-1a"
+#   availability_zone = each.value
 # }
+#
+# # resource "aws_rds_cluster_instance" "aurora-postgresql-cluster" {
+# # #   for_each = var.az
+# #   identifier = "instance-us-east-1a"
+# #   cluster_identifier = aws_rds_cluster.aurora-postgresql-cluster.id
+# #   instance_class     = "db.serverless"
+# #   engine             = aws_rds_cluster.aurora-postgresql-cluster.engine
+# #   engine_version     = aws_rds_cluster.aurora-postgresql-cluster.engine_version
+# #   availability_zone = "us-east-1a"
+# # }
--- aws-resources/14_sqs..tf
+++ aws-resources/14_sqs..tf
@@ -1,12 +1,12 @@
-resource "aws_sqs_queue" "sqs_iaac" {
-  name = "sqs_queue"
-  delay_seconds = 9
-  max_message_size = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  tags = {
-    name="terraform project"
-    description = "managed by terraform provisioning"
-  }
-}
-
+# resource "aws_sqs_queue" "sqs_iaac" {
+#   name = "sqs_queue"
+#   delay_seconds = 9
+#   max_message_size = 2048
+#   message_retention_seconds = 86400
+#   receive_wait_time_seconds = 10
+#   tags = {
+#     name="terraform project"
+#     description = "managed by terraform provisioning"
+#   }
+# }
+#
